04254ba79e4ec5f4824210183f6a7b7b88fda2d4
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-04-11 19:10:19 -0700

    remove the redundant code from express-jwt & jwks-rsa

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const jwt = require("express-jwt")
-const jwks = require("jwks-rsa")
 const { getSession } = require("next-auth/react")
-
-// const { withApiAuthRequired, getSession } = require("@auth0/nextjs-auth0")
-
-// const jwtCheck = jwt({
-//   secret: jwks.expressJwtSecret({
-//     cache: true,
-//     rateLimit: true,
-//     jwksRequestsPerMinute: 5,
-//     jwksUri: "https://dev-fh2bo4e4.us.auth0.com/.well-known/jwks.json",
-//   }),
-//   audience: "http://localhost:5000",
-//   issuer: "https://dev-fh2bo4e4.us.auth0.com/",
-//   algorithms: ["RS256"],
-// })
 
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
