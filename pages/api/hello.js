f811081be8ac2a8ed272d15db619df743fa05c2b
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-04-14 12:48:26 -0700

    fix another typo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import withAuth from "../../middleware/withAuth"
+import withAuth from "../../middlewares/withAuth"
 
 async function Hello(_, res) {
   return res.status(200).json({ name: "Deck API" })
