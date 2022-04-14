ca2206562f6a3730c8283da858670857417f6ad8
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-04-14 12:46:46 -0700

    fix a path error in pages/api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import withAuth from "../middleware/withAuth"
+import withAuth from "../../middleware/withAuth"
 
-async function Hello(req, res) {
+async function Hello(_, res) {
   return res.status(200).json({ name: "Deck API" })
 }
 
